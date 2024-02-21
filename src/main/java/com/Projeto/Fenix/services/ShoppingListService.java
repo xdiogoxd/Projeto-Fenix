0ca793a0a104c26b7ca8ff9a25ef0cb1a1474001
Didi <xdiogoxd@hotmail.com>
2024-02-20 21:33:39 -0300

    criação de funções para adicionar itens na shopping list, feature não foi testado ainda

--- src/main/java/com/Projeto/Fenix/services/ShoppingListService.java
+++ src/main/java/com/Projeto/Fenix/services/ShoppingListService.java
@@ -9,6 +9,7 @@ import com.Projeto.Fenix.repositories.ListMembersRepository;
 import com.Projeto.Fenix.repositories.ShoppingListDetailsRepository;
 import com.Projeto.Fenix.repositories.ShoppingListRepository;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -72,8 +73,14 @@ public class ShoppingListService {
     }
 
     void addItemToList(Users requester, ShoppingList shoppingList, Item theItem, double quantity){
-        // checa se o requester tem autorização para adicionar item na lista
-        if(true){
+        // checa a role do requester nesta lista, para verificar se o usuário pode realizar a atividade ou não
+
+        if (validateShoppingListToUser(requester, shoppingList)){
+
+            if(validateItemAlreadyExist(theItem, shoppingList) ){
+
+
+            }
 
         }
 
@@ -95,5 +102,79 @@ public class ShoppingListService {
 
     void removeItemToList(Users requester, String listId, Item theItem, double quantity){
         //shoppingListDetailsRepository.addItem(requester.getUserId(), listId, theItem.getItemId(), quantity);
+    }
+
+    boolean validateShoppingListToUser(Users requester, ShoppingList shoppingList){
+
+        TypedQuery<ListMembers> theQuery = entityManager.createQuery(
+                "FROM ListMembers WHERE listId=:theList AND memberId=:theMember ", ListMembers.class);
+
+        theQuery.setParameter("theList",shoppingList.getListId());
+        theQuery.setParameter("theMember",requester.getUserId());
+
+        ListMembers theMember = theQuery.getSingleResult();
+
+        if (theMember.getListRole() != "Admin"){
+            return false;
+        }else {
+            return true;
+        }
+
+    }
+
+    boolean validateItemAlreadyExist(Item theItem, ShoppingList shoppingList){
+        TypedQuery<ShoppingListDetails> theQuery = entityManager.createQuery(
+                "FROM ShoppingListDetails WHERE listId=:theList AND itemId=:theItem", ShoppingListDetails.class);
+
+        theQuery.setParameter("theList", shoppingList.getListId());
+        theQuery.setParameter("theItem", theItem.getItemId());
+
+        ShoppingListDetails theShoppingListDetails = theQuery.getSingleResult();
+
+        if (theShoppingListDetails.equals(null)){
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+    String updateItems(ShoppingList theList, Item theItem, double quantity, String action) throws Exception {
+
+        if(validateItemAlreadyExist(theItem, theList)){
+            if(action == "add"){
+                TypedQuery<ShoppingListDetails> theQuery = entityManager.createQuery(
+                        "FROM ShoppingListDetails WHERE listId=:theList AND itemId=:theItem", ShoppingListDetails.class);
+
+                theQuery.setParameter("theList", theList.getListId());
+                theQuery.setParameter("theItem", theItem.getItemId());
+
+                ShoppingListDetails theShoppingListDetails = theQuery.getSingleResult();
+
+                theShoppingListDetails.setItemQuantity(theShoppingListDetails.getItemQuantity() + quantity);
+
+                entityManager.merge(theShoppingListDetails);
+                return "Item updated";
+            }else if (action == "remove"){
+                TypedQuery<ShoppingListDetails> theQuery = entityManager.createQuery(
+                        "FROM ShoppingListDetails WHERE listId=:theList AND itemId=:theItem", ShoppingListDetails.class);
+
+                theQuery.setParameter("theList", theList.getListId());
+                theQuery.setParameter("theItem", theItem.getItemId());
+
+                ShoppingListDetails theShoppingListDetails = theQuery.getSingleResult();
+
+                if (theShoppingListDetails.getItemQuantity() < quantity){
+                    entityManager.remove(theShoppingListDetails);
+                    return "Item removed";
+                }
+                theShoppingListDetails.setItemQuantity(theShoppingListDetails.getItemQuantity() - quantity);
+
+                entityManager.merge(theShoppingListDetails);
+
+            } else {
+                throw new Exception("Ação não suportada");
+            }
+        }
+        throw new Exception("Item não está na lista");
     }
 }
